a997a368e3a816d7c396d2acdca983705d0c5d87
Lukas Suess <lsuess97@gmail.com>
2021-06-08 12:41:28 -0700

    start smooth scroll v1

---
+++ app.js
@@ -0,0 +1,7 @@
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect();
+    
+}
+
+smoothScroll('.section2', 1000);
